e3f4b7f996a856b34b58dc71511ea832cc58de47
Matias <mjannello@gmail.com>
2021-09-30 00:46:41 -0300

    Add schemas and serializers

--- web/views/products_views.py
+++ web/views/products_views.py
@@ -1,11 +1,16 @@
 from flask_restplus import Resource
+from marshmallow import Schema, fields
 
 from web.api import api
+from web.views.serializers import add_product_fields, error_fields
 
 ns_products = api.namespace('products', description='Product Resource')
 
 
 @ns_products.route('/')
 class ProductsCollection(Resource):
-    def get(self):
+    @ns_products.response(201, 'Success')
+    @ns_products.response(400, 'Bad Request', error_fields)
+    @api.expect(add_product_fields, validate=True)
+    def post(self):
         return 'first endpoint working', 200
---
+++ web/views/serializers.py
@@ -0,0 +1,23 @@
+from flask_restplus import fields
+
+from web.api import api
+
+add_product_fields = api.model(
+    'Add product fields',
+    {
+        'id': fields.String(example="MLB12345"),
+        'storage': fields.String(example="AR01"),
+        'location': fields.String(example="LM-00-00-IZ"),
+        'quantity': fields.Integer(example=10)
+    }
+)
+
+error_fields = api.model(
+    'Error',
+    {
+        'title': fields.String(example="Not Found"),
+        'message': fields.String(example="The requested resource was not found"),
+        'status_code': fields.String(example="404"),
+        'error_code': fields.Integer(example='NOT-FOUND')
+    }
+)
