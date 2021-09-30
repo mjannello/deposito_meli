9e356988569c50ba8b726121677ae3005ad0e4bf
Matias <mjannello@gmail.com>
2021-09-30 01:36:09 -0300

    Add schema validation for input location

--- web/schemas.py
+++ web/schemas.py
@@ -1,4 +1,9 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validates, validate
+
+import re
+# r = re.compile("^[A-Z]{2}-\d{2}-\d{2}-(DE|IZ)$")
+# if r.match('x/x/xxxx xx:xx') is not None:
+#    print 'matches'
 
 
 class AddProduct(Schema):
@@ -6,3 +11,9 @@ class AddProduct(Schema):
     storage = fields.String(required=True)
     location = fields.String(required=True)
     quantity = fields.Integer(required=True)
+
+    @validates('location')
+    def validate_location(self, value):
+        r = re.compile("^[A-Z]{2}-\d{2}-\d{2}-(DE|IZ)$")
+        if not r.match(value):
+            raise ValueError
